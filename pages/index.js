177174eeccb3781894d1ac73dd1580672f0da3ad
tanku_for_gitBash <57029371+tanku3080@users.noreply.github.com>
2022-01-28 00:21:25 +0900

    indexcodechange

--- pages/index.js
+++ pages/index.js
@@ -5,12 +5,9 @@ import Footer from '@components/Footer'
 export default function Home() {
   return (
     <div className="container">
-      
+
       <main>
         <Header title="Welcome to tanku room!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
       </main>
 
       <Footer />
