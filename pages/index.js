b46efe75a9a5b4d4157579c65419caefbfb1790f
tanku_for_gitBash <57029371+tanku3080@users.noreply.github.com>
2022-01-28 00:33:30 +0900

    change

--- pages/index.js
+++ pages/index.js
@@ -12,9 +12,6 @@ export default function Home() {
 
       <main>
         <Header title="Welcome to tanku room!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
       </main>
 
       <Footer />
